06f28ac727dc4da6306c19001c58af14d345301e
Cheol-Cheol <dream174822@gmail.com>
2022-01-14 10:26:45 +0900

    📝  JS 적용 테스트

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/* <a href="./post.html">
+  <li>
+    <div class="notice-list__num">1</div>
+    <div class="notice-list__title">첫 게시글 업로드</div>
+    <div class="notice-list__date">21-12-30</div>
+  </li>
+</a>; */
+
+function App() {
+  console.log("hi");
+}
+App();
